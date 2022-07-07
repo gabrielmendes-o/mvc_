9a6dccea3f6eebd4b4bf4eeeb907f14098ba2a36
Gabriel Henrique Mendes de Oliveira <goliveira@secullum.intra>
2022-07-07 10:20:44 -0300

    alterando

--- WebMVC/Startup.cs
+++ WebMVC/Startup.cs
@@ -19,6 +19,8 @@ namespace WebMVC
             Configuration = configuration;
         }
 
+
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
